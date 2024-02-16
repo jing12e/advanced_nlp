49cad93dd5aa51b9e6269508543688272f9e2d4e
katkoe <93648095+katkoe@users.noreply.github.com>
2024-02-16 13:10:39 +0100

    Update featurize.py

--- featurize.py
+++ featurize.py
@@ -1,3 +1,5 @@
+#requires GloVe.6B.{n}D.txt to be present in path
+
 from sklearn.preprocessing import  OneHotEncoder
 import numpy as np
 import spacy
@@ -20,24 +22,33 @@ def preprocess(features, labels, load_pickle=False, pickle_name='training'):
     #labels are changed here as well to match the slightly different tokenization method the featurization model uses
     for doc in tqdm(docs, disable=silent):
         if i < len(features):
-            token = doc[0]
-            processed_features.append({
-                "TEXT": token.text,
-                "LEMMA": token.lemma_,
-                "POS": token.pos_,
-                "TAG": token.tag_,
-                "DEP": token.dep_,
-                "SHAPE": token.shape_,
-                "ALPHA": token.is_alpha,
-                "STOP": token.is_stop
-            })
+            for token in doc:
+                current_token = token
+                path_length = 0
+                print(token.dep_)
+                while current_token.dep_ != 'ROOT':
+                    current_token = current_token.head
+                    path_length += 1
+                processed_features.append({
+                    "TEXT": token.text,
+                    "LEMMA": token.lemma_,
+                    "POS": token.pos_,
+                    "TAG": token.tag_,
+                    "DEP": token.dep_,
+                    "SHAPE": token.shape_,
+                    "ALPHA": token.is_alpha,
+                    "STOP": token.is_stop,
+                    "HEAD": token.head.text,
+                    "CHILD": [child.text for child in token.children],
+                    "PATH_LEN": path_length
+                })
             new_labels.append(labels[i])
             i += 1
     return processed_features, new_labels
 
-training_string = 'This is my favourite test string.'
-label_string = 'Until I saw this fabulous string!'
+training_string = """The sun rose slowly over the horizon, casting its golden glow across the tranquil landscape. Birds chirped melodiously, welcoming the new day with their joyful songs. In the distance, a gentle breeze rustled through the leaves of the trees, carrying with it the promise of adventure."""
+label_string = training_string
 training_features = [{'token' : i} for i in training_string.split(' ')]
 gold_labels = [{'token' : i} for i in label_string.split(' ')]
 training_features,gold_labels = preprocess(training_features, gold_labels, load_pickle=False)
-print(f"training features:{training_features}")
+print(f"training features:\n{[(token['HEAD'],token['CHILD'],token['PATH_LEN']) for token in training_features]}")
