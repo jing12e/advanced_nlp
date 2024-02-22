53d7258245a767c28956f3d7257b86f004bd647b
katkoe <93648095+katkoe@users.noreply.github.com>
2024-02-22 22:36:22 +0100

    Created generator to reduce ram usage

--- assignment2/fit_inference.py
+++ assignment2/fit_inference.py
@@ -6,13 +6,21 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import classification_report, confusion_matrix
 import pickle
+from tqdm import tqdm
 
-def load_model(pickle):
+def load_model(pickles):
     '''
     Input: string name with .pickle extension. Must be present in working directory. Check with 'pwd' command
     Loads pretrained model variable from pickle file at filepath(pickle).
     '''
-    with open(pickle, 'rb') as p:
+    with open(pickles, 'rb') as p:
+        return pickle.load(p)
+
+def unpicklify(pickles):
+    '''
+    Loads a variable from a pickle file
+    '''
+    with open(pickles, 'rb') as p:
         return pickle.load(p)
 
 def picklify(pickles,not_so_pickles):
@@ -54,12 +62,20 @@ def process_args():
     parser.add_argument('--output_file', type=str,
                         default='output/inference.txt',
                         help="Input the path of the output files")
+    
+    parser.add_argument('--load', type=bool,
+                        default=False,
+                        help="Load from generator folder? (needs to be prepopulated)")
     return parser.parse_args()
 
-def create_classifier(x_train, y_train):
-    model = LogisticRegression(max_iter=1000)
-    model.fit(x_train, y_train)
+def create_classifier():
+    model = LogisticRegression(max_iter=1000, verbose=True)
     return model
+
+def fit_classifier(x_train, y_train, model):
+    return model.fit(x_train, y_train)
+
+    
 
 def inference(model, x_test, output_file):
     predictions = model.predict(x_test)
@@ -70,15 +86,16 @@ def inference(model, x_test, output_file):
 def evaluate(predictions, true_y):
     report = classification_report(true_y, predictions, output_dict=True)
     conf_matrix = confusion_matrix(true_y, predictions)
-    
+    print(report)
+    print(conf_matrix)
 
 
 # Example usage:
 args = process_args()
-x_train, y_train = pd.read_csv(args.x_train), pd.read_csv(args.y_train)
+x_train, y_train = pd.read_csv(args.x_train), pd.read_csv(args.y_train) #we have one million words to process. It's too long
 skiplist = ['word','predicate_position', 'next_lemma','previous_lemma', 'lemma','path_len']
 t = x_train.at[0,'predicate']
-
+x_train,y_train = x_train, y_train
 x_train['predicate'] = pd.Series([1 if isinstance(pred, str) else 0 for pred in x_train['predicate']])
 
 encoder_dict = {feature: fit_encoder(x_train[feature].values) for feature in x_train.columns[1:] if feature not in skiplist}
@@ -92,10 +109,24 @@ for column in x_train.columns[1:]:
         pass
     elif column in ['next_lemma', 'previous_lemma']:
         pass
-        
+
 x_train_encoded = np.concatenate(encoded_x_train, axis=1)
 y_train = y_train['argument']
-model = create_classifier(x_train_encoded, y_train)
+print(f'### Populating Generator folder ###\n')
+if not args.load:
+    for i in tqdm(range(100)):
+        picklify(f'generator_folder/x_train_{i}.pickle',x_train[int(len(x_train)/100)*(i-1):int(len(x_train)/100)*i])
+        picklify(f'generator_folder/y_train_{i}.pickle',y_train[int(len(x_train)/100)*(i-1):int(len(x_train)/100)*i])
+x_train, y_train = 0, 0
+model = create_classifier()
+
+print(f'### Training LogReg Model ###\n')
+for i in tqdm(range(100)):
+    x_train, y_train = np.array(unpicklify(f'generator_folder/x_train_{i}.pickle')), np.array(unpicklify(f'generator_folder/y_train_{i}.pickle'))
+    if len(x_train) > 5:
+        model = fit_classifier(x_train,y_train,model)
+        picklify('logreg.pickle',model)
+
 
 
 x_test, y_test = pd.read_csv(args.x_test), pd.read_csv(args.y_test)
@@ -109,6 +140,5 @@ y_test = y_test['argument']
 
 predictions = inference(model, x_test_encoded, args.output_file)
 
-picklify(model)
 
 evaluate(predictions, y_test)
