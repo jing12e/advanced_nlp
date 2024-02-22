651f9ba505009c3e649637359556bcfcecf5d01e
katkoe <93648095+katkoe@users.noreply.github.com>
2024-02-22 13:34:55 +0100

    basic classification script

---
+++ assignment2/fit_inference.py
@@ -0,0 +1,46 @@
+from sklearn.linear_model import LogisticRegression
+
+def process_args():
+    """
+    Processes user arguments from the command prompt. 
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_x_train', type=str, 
+                        default='training_dataset_x.csv',
+                        help="Input the relative path in the folder to the csv containing featurizes for the training set")
+    
+    parser.add_argument('--input_y_train', type=str, 
+                        default='training_dataset_y.csv',
+                        help="Input the relative path in the folder to the csv containing the y labels for the training set")
+    
+    parser.add_argument('--input_x_test', type=str, 
+                        default='test_dataset_x.csv',
+                        help="Input the relative path in the folder to the csv containing featurizes for the test set")
+    
+    parser.add_argument('--input_y_test', type=str, 
+                        default='test_dataset_y.csv',
+                        help="Input the relative path in the folder to the csv containing the y labels for the test set")
+    
+    parser.add_argument('--output_file', type=str, 
+                        default='output/inference.txt',
+                        help="Input the path of the output files")
+    return parser.parse_args()
+
+
+def create_classifier(x_train, y_train):
+    model = LogisticRegression(max_iter=1000) #can remove max_iter if the model converges properly
+    x_train, y_train = x_train.to_dict(orient='records'), y_train.to_dict(orient='records') 
+    model.fit(x_train, y_train)
+    return model
+
+def inference(model, x_test, y_test, output_file):
+    predictions = model.predict(x_test)
+    with open(output_file, 'w') as file:
+        [file.write(f"{prediction}\n") for prediction in predictions]
+    return predictions
+
+def evaluate(prediction, true_y):
+    pass
+    # some evaluation
+
+model = create_classifier(x_train, y_train)
